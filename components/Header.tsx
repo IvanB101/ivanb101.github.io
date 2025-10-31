7bb976ec4626ea8f55b01c6814b8dcc903b49249
IvanB101 <104206321+IvanB101@users.noreply.github.com>
2025-10-31 20:25:27 +0100

    fixed header alignment

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,10 @@ export default async function Header({ lang }: { lang: SupportedLanguages }) {
   return (
     <div id="home" className="w-full flex flex-col items-center">
       <div className="flex flex-col gap-4 md:gap-10 items-center h-screen justify-center">
-        <div className="text-5xl md:text-7xl lg:text-8xl font-archivo">
+        <div className="text-5xl md:text-7xl lg:text-8xl font-archivo text-center px-5">
           Ivan Brocas
         </div>
-        <div className="text-xl md:text-2xl lg:text-3xl">
+        <div className="text-xl md:text-2xl lg:text-3xl text-center px-5">
           {dictionary.title}
         </div>
       </div>
